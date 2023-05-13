fe4ab73adc9b239ec0ca57479812d91595d68be3
RussianBoy64 <kozlachkovvd@gmail.com>
2023-05-14 00:10:36 +0400

    feat: add info about myself

--- src/utils/aboutStudents.ts
+++ src/utils/aboutStudents.ts
@@ -1,14 +1,21 @@
 import PhotoAleksandra from "../assets//images/Aleksandra.jpg";
+import PhotoVladimir from "../assets//images/Vladimir.jpg";
 
 export const aboutStudent = [
   {
     name: "Alekdandra Bulova",
     profession: "Front End Developer",
     photo: PhotoAleksandra,
-    decription: `I&apos;m a frontend developer. I&apos;m young ambitious used to go to
+    decription: `I\`m a frontend developer. I\`m young ambitious used to go to
     my goal. I like to work in a team. Now I want to spend all my strength
-    and energy on training and developing my professional skills. I&apos;m
-    ready to work hard, study and improve myself. I&apos;m sociable easy
+    and energy on training and developing my professional skills. I\`m
+    ready to work hard, study and improve myself. I\`m sociable easy
     to train hardworking accurate diligent attentive to details.`,
+  },
+  {
+    name: "Vladimir Kozlachkov",
+    profession: "Front End Developer",
+    photo: PhotoVladimir,
+    decription: `I\`am frontend developer from Saratov. I like to play football, travel and drink beer. My dream is to become sucsessfull senior developer and create usefull aplications that help people make it\`s life better.`,
   },
 ];
